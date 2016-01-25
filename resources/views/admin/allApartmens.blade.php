25078456a8652589e93576db49a67b9f4b146262
aleksandar <cojashc@gmail.com>
2016-01-25 18:09:36 +0100

    minor

--- resources/views/admin/allApartmens.blade.php
+++ resources/views/admin/allApartmens.blade.php
@@ -1,7 +1,7 @@
 <div class="col-lg-12 col-md-12 col-xs-12 allApartments" ng-hide="hideApartments">
 	<span id="title">Svi vasi apartmani <hr></span>
 	<div id="all" ng-repeat="item in apartments">
-		<div ng-repeat="img in item.images" ng-style="{'background-image':'url(../images/'+img.image_name+')'}" 
+		<div ng-repeat="img in item.images|limitTo:1" ng-style="{'background-image':'url(../images/'+img.image_name+')'}" 
 			class="col-lg-2 col-md-2 col-xs-12 box">
 			<p> [[ item.apartment_name ]] </p>
 			<p> Cena : [[ item.price ]] &euro; </p>
